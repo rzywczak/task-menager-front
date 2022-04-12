49bb0e683cd0d3dbd168ad741b15c1bd604195f3
rzywczak <73279676+rzywczak@users.noreply.github.com>
2022-04-12 22:40:17 +0200

    deployment heroku change

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue'
 import RegisterForm from '../views/RegisterForm.vue'
 import LoginForm from '../views/LoginForm.vue'
 
-
+ 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
