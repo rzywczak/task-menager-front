af24e257b5d54fdf6acd462e5356c7e89c845b91
rzywczak <73279676+rzywczak@users.noreply.github.com>
2022-04-20 20:03:15 +0200

    modify .env file add .env.example file

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
 
-// const url = 'https://rzywczak-task-menager.herokuapp.com/api'
-
-axios.defaults.baseURL = VUE_APP_API_URL
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL
 axios.defaults.headers.common['Authorization'] = 'Bearer '+ localStorage.getItem('token')
